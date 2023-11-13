ea9409a0e1d9664d5e754017cb139506cc7bc29f
Kevin Huang <huan2005@purdue.edu>
2023-11-12 21:10:40 -0500

    Refine camper.js

--- camper.js
+++ camper.js
@@ -1,12 +1,25 @@
 const axios = require('axios');
+var count = 0;
 
+const aggressive = 10000;
+const moderate = 15000;
+const normal = 20000;
+const chill = 30000;
+
+const interval = normal; // ms
+
+// Audio stuff
 const { exec } = require('child_process');
-const vlcExecutable = ''; // Change this to the VLC executable path (vlc.exe)
-const audio = ''; // Change this to the audio file path
+const vlcExecutable = ''; // VLC executable path here
+const audio = ''; // Audio file path here
 const command = `"${vlcExecutable}" ${audio}`;
 
+
+// Cookie stuff
 const cookies = [
-    '', // Add all relevant cookies here
+    'cookie1=cookie1data',
+    'cookie2=cookie2data',
+    'etc=etcdata', // Copy and paste your cookies here
 ];
 
 const cookieHeader = cookies.join('; ');
@@ -17,12 +30,14 @@ const headers = {
 };
 
 
+// Main logic
 async function checkSite(url, keyword, headers) {
     while (true) {
         try {
             const response = await axios.get(url, headers);
             const data = response.data;
             const keywordPattern = new RegExp(keyword, 'i');
+            const invalidKeyword = new RegExp('UNAUTHENTICATED', 'i');
 
             if (keywordPattern.test(data)) {
                 console.log(`A wild "${keyword}" has appeared!!!`);
@@ -34,20 +49,25 @@ async function checkSite(url, keyword, headers) {
                     console.log('Audio playback completed.');
                 });        
                 break;
-            } else {
-                console.log(`No "${keyword}" yet...`);
+            } else if (invalidKeyword.test(data)) {
+                console.log(`Not signed in! Please update your cookies.`);
+                break;
+            } else{
+                console.log(`No "${keyword}" yet... ${count++}`);
             }
 
-            await sleep(10000);
+            await sleep(interval);
         } catch (error) {
             console.log('Error making HTTP request:', error);
-            await sleep(10000);
+            await sleep(interval);
         }
     }
 }
 
 
-const targetUrl = ''; // Change this to a target URL
-const keyword = ''; // Change this to a keyword to search for
+// Camp away!
+const targetUrl = ''; // URL here
+const keyword = ''; // Keyword here
 const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
 checkSite(targetUrl, keyword, headers);
