4bb34c102f06929729e7fd30d70d31c663c82958
Kevin Huang <huan2005@purdue.edu>
2023-11-16 12:06:46 -0500

    Add time

--- src/camper_discord.js
+++ src/camper_discord.js
@@ -1,9 +1,8 @@
 const axios = require('axios');
 const { Client, GatewayIntentBits } = require('discord.js');
-var count = 0;
 
 const targetUrl = ''; // URL here
-const keywords = ['', '']; // Keywords here
+const keywords = ['', '', '']; // Keywords here (multiple supported)
 
 const aggressive = 10000;
 const moderate = 15000;
@@ -16,13 +15,13 @@ const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 const axiosInstance = axios.create();
 const keywordPatterns = keywords.map(keyword => new RegExp(keyword, 'i'));
-const invalidKeyword = new RegExp('UNAUTHENTICATED', 'i');
+const invalidKeyword = new RegExp('UNAUTHENTICATED', 'i'); // Set an keyword that indicates that you are not logged in
 
 // Cookie stuff
 const cookies = [
-    'cookie1=cookie1data',
-    'cookie2=cookie2data',
-    'etc=etcdata', // Copy and paste your cookies here
+    'cookie1=cookie1value',
+    'cookie2=cookie2value',
+    'etc=etcvalue' // Add necessary cookies here
 ];
 
 const cookieHeader = cookies.join('; ');
@@ -48,41 +47,42 @@ async function checkSite(url, keywords, headers) {
             const data = responses.map(response => response.data);
             keywordFound = keywordPatterns.some(pattern => pattern.test(data[0]));
 
+            const currentTime = new Date();
+            const formattedTime = currentTime.toTimeString().split(' ')[0]; // HH:MM:SS
+
             if (keywordFound) {
                 if (first) {
                     // Keyword has been found for the first time
-                    console.log(`A wild "${keywords.join('" or "')}" has appeared!!!`);
-                    sendDiscordMessage(client, channelId, `A wild "${keywords.join('" or "')}" has appeared!!!`);
+                    console.log(`[${formattedTime}] A wild "${keywords.join('" or "')}" has appeared!!!`);
+                    sendDiscordMessage(client, channelId, `[${formattedTime}] A wild "${keywords.join('" or "')}" has appeared!!!`);
                     first = false;
-                    count = 0;
                 } else {
                     // Keyword is still present
-                    console.log(`Already found "${keywords.join('" or "')}". Go to the site now! ${count++}`);
+                    console.log(`[${formattedTime}] Already found "${keywords.join('" or "')}". Go to the site now!`);
                 }
                 wasKeywordFound = true;
             } else if (invalidKeyword.test(data[0])) {
                 console.log(`Not signed in! Please update your cookies.`);
-                sendDiscordMessage(client, channelId, `Login expired, shutting down...`);
-                
+                // sendDiscordMessage(client, channelId, `[${formattedTime}] Login expired, shutting down...`);
+
                 await Promise.race([
                     sleep(3000),
                     new Promise((_, reject) => setTimeout(() => reject(new Error('Timeout')), 3000)),
                 ]);
                 process.exit();
             } else if (!keywordFound && wasKeywordFound) {
-                console.log(`"${keywords.join('" or "')}" has disappeared.`);
-                sendDiscordMessage(client, channelId, `"${keywords.join('" or "')}" has disappeared :(`);
+                console.log(`[${formattedTime}] "${keywords.join('" or "')}" has disappeared.`);
+                sendDiscordMessage(client, channelId, `[${formattedTime}] "${keywords.join('" or "')}" has disappeared  :sob:`);
                 first = true;
-                count = 0;
                 wasKeywordFound = false;
             } else {
-                console.log(`No "${keywords.join('" or "')}" yet... ${count++}`);
+                console.log(`[${formattedTime}] No "${keywords.join('" or "')}" yet...`);
                 wasKeywordFound = false;
             }
 
             await sleep(interval);
         } catch (error) {
-            console.log('Error making HTTP request:', error);
+            console.log('Error making HTTP request:', error.message);
             await sleep(interval);
         }
     }
@@ -103,7 +103,7 @@ function sendDiscordMessage(client, channelId, message) {
 
 // Discord stuff
 const token = ''; // Discord bot token here
-const channelId = ''; // Discord channel ID here
+const channelId = ''; // Channel ID here
 const client = new Client({
     intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
 });
